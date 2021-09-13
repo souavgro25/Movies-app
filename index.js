c7f9de38664dbbe87d62b12a44796dce714d61e9
sourabh grover <souravgro25@gmail.com>
2021-09-13 23:42:01 +0530

    added new features

--- index.js
+++ index.js
@@ -4,21 +4,31 @@ const Searchmovie = document.querySelector("#searchmovies");
 const MOVIE_DB_API = "d8bf019d0cca372bd804735f172f67e8";
 const MOVIE_DB_ENDPOINT = "https://api.themoviedb.org";
 const MOVIE_DB_IMAGE_ENDPOINT = "https://image.tmdb.org/t/p/w500";
+const Menu = document.querySelector("#menu");
+const Toggle = document.querySelector("#toggle");
 const Search = document.querySelector("#search");
-
+const Heading = document.querySelector("#heading");
+const Related = document.querySelector("#related");
 Form.addEventListener("submit", (e) => {
   e.preventDefault();
   searchMovie(Search.value);
 });
 
-function getmovies(url, render, id) {
+Toggle.addEventListener("click", togglebar);
+
+function togglebar() {
+  Menu.classList.toggle("hidden");
+}
+
+function getmovies(url, render, id, name) {
   fetch(url)
     .then((res) => res.json())
     .then((res) => {
       let out = "";
-      out = render(res, Searchmovie);
+      out = render(res);
 
       id.innerHTML = out;
+      Heading.innerText = name;
     })
     .catch((err) => console.log(err));
 }
@@ -31,15 +41,32 @@ function generateMovieDBUrl(path) {
 function getTrendingMovies() {
   const url = generateMovieDBUrl("/trending/movie/day");
 
-  getmovies(url, view, Movies);
+  getmovies(url, view, Movies, "Trending Movies");
 }
 
-function view(data, id) {
+function getPopularMovies() {
+  const url = generateMovieDBUrl("/movie/popular");
+
+  getmovies(url, view, Movies, "Popular Movies");
+}
+
+function getTopratedMovies() {
+  const url = generateMovieDBUrl("/movie/top_rated");
+
+  getmovies(url, view, Movies, "Top-Rated Movies");
+}
+
+function getupcomingMovies() {
+  const url = generateMovieDBUrl("/movie/upcoming");
+
+  getmovies(url, view, Movies, "Upcoming Movies");
+}
+
+function view(data) {
   let out = "";
 
   data.results.forEach((data) => {
-    console.log(data);
-    out += `<div onclick="getdetails('${data.id}')" class="flex  flex-col rounded-md bg-gray-900 p-1 justify-center items-center max-w-xs border border-green-500">
+    out += `<div onclick="getdetails('${data.id}')" class="flex flex-none  flex-col rounded-md bg-gray-900 p-1 justify-center items-center max-w-xs border border-green-500">
     <img class="w-60 h-80 rounded" src=${MOVIE_DB_IMAGE_ENDPOINT}${data.poster_path} alt="">
     <h2 style="max-width:15rem" class="text-green-500 font-bold font-serif text-xl text-center mt-2">${data.title}</h2>
     </div>`;
@@ -49,31 +76,29 @@ function view(data, id) {
 function searchMovie(query) {
   const url = generateMovieDBUrl("/search/movie") + "&query=" + query;
   // const url = generateMovieDBUrl(`/movie/${data}`);
-  getmovies(url, view, Searchmovie);
+  getmovies(url, view, Searchmovie, `Searched "${query}"`);
   Movies.classList.add("hidden");
 }
 
 function getdetails(data) {
-  console.log(data);
   sessionStorage.setItem("movie_id", data);
   window.location = "movies.html";
 }
 
 function getmovie() {
   let id = sessionStorage.getItem("movie_id");
-  console.log(id);
+
   const url = generateMovieDBUrl(`/movie/${id}`);
   let movie = "";
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       movie = `<div class="flex md:flex-row flex-col gap-y-5 rounded-md bg-gray-900 p-1 justify-between mt-5  ">
       <div style="max-height:30rem" class=" flex-1 flex  justify-center  ">
       <img class="  border border-green-500 rounded" src=${MOVIE_DB_IMAGE_ENDPOINT}${data.poster_path} alt=""></div>
      <div class="flex-1 flex flex-col gap-y-5 "> 
-      <h2  class="text-green-500 font-bold font-serif md:text-5xl text-3xl mt-2">${data.title}</h2>
-      <div class="bg-gray-800 md:mt-10 mt-5 flex flex-col  gap-y-4 pl-5 pt-5">
+      <h2  class="text-green-500 font-bold font-serif md:text-5xl text-3xl mt-2 md:ml-0 ml-2">${data.title}</h2>
+      <div class="bg-gray-800  mt-5 flex flex-col  gap-y-4 pl-5 pt-5">
 
       <p class="max-w-sm text-md  font-mono font-medium text-green-500"><strong class="text-md font-mono text-gray-500">Tagline:</strong>${data.tagline}</p>
       <p class="max-w-sm text-md  font-mono font-medium text-green-500"><strong class="text-md font-mono text-gray-500">Genres:</strong>${data.genres[0].name}</p>
@@ -84,13 +109,26 @@ function getmovie() {
       </div>
       </div>
       <div class="bg-gray-800 pl-5 py-5"> 
-        <h1 class="text-gray-500 text-xl font-mono">OverView<h1>
+        <h1 class="text-gray-500 text-xl font-medium font-mono">OverView:<h1>
         <p class="text-md max-w-xl mt-2 text-green-500">${data.overview}</p>  
       </div>
       </div>`;
       document.querySelector("#Details").innerHTML = movie;
     });
-  console.log(movie);
+}
+
+function getrelated() {
+  let id = sessionStorage.getItem("movie_id");
+  const url = generateMovieDBUrl(`/movie/${id}/similar`);
+  getmovies(url, view, Related);
 }
 
 getTrendingMovies();
+document
+  .querySelector(".upcoming")
+  .addEventListener("click", getupcomingMovies);
+document.querySelector(".Top").addEventListener("click", getTopratedMovies);
+document.querySelector(".Popular").addEventListener("click", getPopularMovies);
+document
+  .querySelector(".Trending")
+  .addEventListener("click", getTrendingMovies);
